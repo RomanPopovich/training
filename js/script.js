8b095cac9efac1a8b8c50468356d885be3a4b151
Roman <roma.popovich95@mail.ru>
2022-08-25 00:26:35 +0300

    quest3

--- js/script.js
+++ js/script.js
@@ -292,9 +292,104 @@
 
 // Пример 8 
 // С помощью цикла найдите сумму чисел arr = [2,5,8,3,6]
-let result = 0;
-let arr = [2,5,8,3,6];
-for(let i = 0; i < arr.length; i++){
-    result = result + arr[i];
-}
-console.log(result);
+// let result = 0;
+// let arr = [2,5,8,3,6];
+// for(let i = 0; i < arr.length; i++){
+//     result = result + arr[i];
+// }
+// console.log(result);
+// for(let i = 0; i <=16; i++){
+//     if(i % 2 == 2){
+//         console.log(i)
+//     }
+// }
+
+
+// Домашнее задание
+// Выписать в комментариях. Вам необходимо найти все переменные,
+// функции, аргументы и параметры.
+// Запишите имена в соответсвующие пункты
+// Переменные: myDog, guests   переменная years локальная
+// Функции: dogYears, makeTea, secret
+// Встроенные функции: console.log
+// Аргументы: (myDog, 4), (guests, 'Earl Grey')
+// Параметры: (dogname, age), (cups, tea)
+
+// function dogYears(dogname, age){
+//     let years = age * 2;
+//     console.log(dogName + ' is ' + years + ' years old');
+// }
+// let myDog = 'Rex';
+// dogYears(myDog, 4);
+
+// function makeTea (cups, tea){
+//     console.log('Breawing ' + cups + ' cups of' + tea);
+// }
+// let guests = 2;
+// makeTea(guests, 'Earl Grey');
+
+// function secret(){
+//     console.log('The secret of life is 28');
+// }
+// secret();
+
+
+// Дана строка 'JS'. Сделайте из нее строку 'js'.
+
+// let txt = "JS";
+// console.log(txt.toLowerCase());
+
+// Дана строка 'я люблю JS!'. Вырежите из нее слово 
+// 'люблю' и слово 'javascript' тремя разными способами 
+// (через substr, substring, slice).
+
+let txt = 'я люблю JS!';
+console.log(txt.substring(2,7), txt.substring(7,10));
+console.log(txt.substr(2,6), txt.substr(-3,2));
+console.log(txt.slice(2,8), txt.slice(-3,-1));
+
+// Дана строка 'я люблю JS!'. Найдите позицию 
+// подстроки 'люблю'.
+
+// let txt = 'я люблю JS!';
+// console.log(txt.search('люблю'));
+
+// Дана переменная txt, в которой хранится какой-либо текст. 
+// Реализуйте обрезание длинного текста по следующему 
+// принципу: если количество символов этого текста больше 
+// заданного в переменной n, то в переменную result запишем 
+// первые n символов строки txt и добавим в конец 
+// троеточие '...'. В противном случае в переменную 
+// result запишем содержимое переменной txt.
+
+// let txt = "в которой хранится какой-либо текст";
+// let n = 16;
+// if(n < txt.length){
+// let result = txt.slice(0,18) + " ...";
+// console.log(result)
+// }else{
+// let result = txt;
+// console.log(result)
+// }
+
+// Для решения задач данного блока вам понадобятся следующие 
+// методы: replace.
+// Дана строка 'Я-люблю-JS!'. 
+// Замените все  '-' на '!' с помощью глобального 
+// поиска и замены.
+
+// let txt = 'Я-люблю-JS!';
+// console.log(txt.replace(/-/g, '!'));
+
+// Дана строка 'я люблю JS'. С помощью метода split 
+// запишите каждое слово этой строки в отдельный элемент 
+// массива.
+
+// let txt = 'Я-люблю-JS!';
+// console.log(txt.split(""));
+
+// Дана строка 'привет мир'. С помощью метода split 
+// запишите каждый символ этой строки в отдельный элемент 
+// массива.
+// let txt = 'привет мир';
+// console.log(txt.split(""));
